2082bcac605ed757f78a57456308d430b861c1ac
Mohit <mohittt0011@gmail.com>
2026-05-19 15:00:33 +0530

    fix: resolve react router v7 transition and splat route resolution warnings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <ThemeProvider>
       <ScanProvider>
-        <Router>
+        <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/scan" element={<InputPage />} />
